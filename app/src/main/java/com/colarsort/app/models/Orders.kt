a6db8c6e5746336394ea4665a2a5fd685c84bbfd
Larusu <ldaneliee33@gmail.com>
2025-11-14 17:47:31 +0800

    Added models for database tables: Users.kt, Products.kt, Materials.kt, ProductMaterials.kt, ProductionStatus.kt, Orders.kt, OrderItems.kt

---
+++ app/src/main/java/com/colarsort/app/models/Orders.kt
@@ -0,0 +1,6 @@
+package com.colarsort.app.models
+
+data class Orders(val id: Integer,
+                  val productId: Integer,
+                  val materialId: Integer,
+                  val quantityRequired: Double)
---
+++ app/src/main/java/com/colarsort/app/models/Products.kt
@@ -0,0 +1,5 @@
+package com.colarsort.app.models
+
+data class Products(val id: Integer,
+                    val name: String,
+                    val image: String)
